de7e1c3dbfdd5a8f826e2d9b0a5fe434553ef6d7
Hector Durazo <hectordurazo1@gmail.com>
2022-09-14 15:27:45 -0700

    graph build i think

--- jsosn.py
+++ jsosn.py
@@ -1,18 +1,66 @@
 import json
 import nltk
+import re
+import networkx as nx
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
+import matplotlib.pyplot as plt
+G = nx.Graph()
+
+
+total =0
+
 nltk.download('stopwords')
 nltk.download('punkt')
-all_stopword=stopword.words('english')
-all_stopwords = stopwords.append(',', '@')
+all_stopwords = stopwords.words('english')
+sw_list = [
+    'id',
+    'like',
+    'feel',
+    'im',
+    'likes',
+    'go',
+]
+all_stopwords.extend(sw_list)
+
+nltk.download('words')
+words = set(nltk.corpus.words.words())
 
 with open('temp.json', 'r') as json_file:
     json_load = json.load(json_file)
 
-
+#list of words to remove if they start with these chars
+rmv_list = ('@', '/', 'http', '1', '3', '4', '5', '6', '7', '8', '9', '0')
+#preproccessing tweets and removing stop words and punctuation
+##still working on removeing @
 for x in range(len(json_load)):
     text = json_load[x]["Source of Tweet"]
-    text_tokens=word_tokenize(text,language="english",preserve_line=True)
-    tokens_without_sw = [word for word in text_tokens if not word in stopwords.words()]
-    print(tokens_without_sw)
+
+    #removes links, twitter handles, and random words that start with numbers
+    result = " ".join(
+        filter(lambda word: not word.lower().startswith(rmv_list),
+               text.split()))
+
+    #removed punctuation
+    result = re.sub(r'[^\w\s]','',result)
+
+    #separates every word and removes all stop words
+    text_tokens = word_tokenize(result.lower())
+    tokens_without_sw = [
+        word for word in text_tokens if not word in all_stopwords
+    ]
+
+    #checks for english words // not sure to add because removes some real english words // about 20k words are removed
+    #tokens_without_sw = [
+    #   w for w in tokens_without_sw if w.lower() in words or not w.isalpha()
+    #]
+    for i in tokens_without_sw:
+        for j in tokens_without_sw:
+            G.add_edge(i,j)
+
+print(G.number_of_nodes())
+nx.draw(G)
+plt.savefig("graph.png")
+#total=total + len(tokens_without_sw)
+#print(total)
+#print(tokens_without_sw)

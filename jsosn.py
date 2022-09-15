d7dba7456680c246132d5bfc173b8b8f599ee209
Hector Durazo <hectordurazo1@gmail.com>
2022-09-14 22:57:28 -0700

    messing with visuals

--- jsosn.py
+++ jsosn.py
@@ -1,15 +1,17 @@
 import json
 from optparse import Option
 import nltk
+import numpy as np
+import collections
 import re
 import networkx as nx
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import matplotlib.pyplot as plt
+
 G = nx.Graph()
 
-
-total =0
+total = 0
 
 nltk.download('stopwords')
 nltk.download('punkt')
@@ -21,8 +23,12 @@ sw_list = [
     'im',
     'likes',
     'go',
-    'dont'
-    '#'
+    'dont',
+    'us',
+    'theyre',
+    'got',
+    'going',
+    '#',
 ]
 all_stopwords.extend(sw_list)
 
@@ -33,10 +39,11 @@ with open('temp.json', 'r') as json_file:
     json_load = json.load(json_file)
 
 #list of words to remove if they start with these chars
-rmv_list = ('@', '/', 'http','#' '1', '3', '4', '5', '6', '7', '8', '9', '0')
+rmv_list = ('@', '/', 'http', '#'
+            '1', '3', '4', '5', '6', '7', '8', '9', '0')
 #preproccessing tweets and removing stop words and punctuation
 ##still working on removeing @
-for x in range(1):
+for x in range(30):
     text = json_load[x]["Source of Tweet"]
 
     #removes links, twitter handles, and random words that start with numbers
@@ -45,7 +52,7 @@ for x in range(1):
                text.split()))
 
     #removed punctuation
-    result = re.sub(r'[^\w\s]','',result)
+    result = re.sub(r'[^\w\s]', '', result)
 
     #separates every word and removes all stop words
     text_tokens = word_tokenize(result.lower())
@@ -55,29 +62,31 @@ for x in range(1):
 
     #checks for english words // not sure to add because removes some real english words // about 20k words are removed
     tokens_without_sw = [
-       w for w in tokens_without_sw if w.lower() in words or not w.isalpha()
+        w for w in tokens_without_sw if w.lower() in words or not w.isalpha()
     ]
     for i in tokens_without_sw:
         for j in tokens_without_sw:
-            G.add_edge(i,j)
+            G.add_edge(i, j)
 G.remove_edges_from(nx.selfloop_edges(G))
 print(G.number_of_nodes())
 
-options = {
-    "font_size": 6,
-    "node_size": 700,
-    "node_color": "white",
-    "edgecolors": "black",
-    "linewidths": 1,
-    "width": 1,
-}
-nx.draw_networkx(G,**options)
+# print degree for each team - number of games
+#for n, d in G.degree():
+#    print('%s %d' % (n, d))
+#
+#options = {
+#    'node_color': 'black',
+#    'node_size': 50,
+#    'linewidths': 0,
+#    'width': 0.1,
+#}
+#nx.draw(G, **options)
+#plt.show()
 
-ax = plt.gca()
-ax.margins(0.10)
-plt.axis("off")
+deg_centrality = nx.degree_centrality(G)
+centrality = np.fromiter(deg_centrality.values(), float)
+# plot
+pos = nx.kamada_kawai_layout(G)
+nx.draw(G, pos, node_color=centrality, node_size=centrality*2e3)
+nx.draw_networkx_labels(G, pos)
 plt.show()
-print(G.number_of_nodes())
-#total=total + len(tokens_without_sw)
-#print(total)
-#print(tokens_without_sw)

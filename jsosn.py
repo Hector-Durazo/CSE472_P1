5579046ff48ffb57016cc471e6d72b65baa0022e
Hector Durazo <hectordurazo1@gmail.com>
2022-09-22 21:56:52 -0700

    Almost done

--- jsosn.py
+++ jsosn.py
@@ -1,98 +1,120 @@
+import re
 import json
 import nltk
-import re
-import scipy as sp
 import numpy as np
+import pandas as pd
 import networkx as nx
+import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-import matplotlib.pyplot as plt
-
-G = nx.Graph()
-
-nltk.download('stopwords')
-nltk.download('punkt')
-all_stopwords = stopwords.words('english')
-sw_list = ['id','like','feel','im','likes','go','dont',
-            'us','theyre','got','going','#']
-all_stopwords.extend(sw_list)
-
-nltk.download('words')
-words = set(nltk.corpus.words.words())
-
-
-with open('temp.json', 'r') as json_file:
-    json_load = json.load(json_file)
-
-#list of words to remove if they start with these chars
-rmv_list = ('@', '/', 'http', '#',
-            '1','2', '3', '4', '5', '6', '7', '8', '9', '0')
+import snscrape.modules.twitter as sntwitter
 
 #preproccessing tweets and removing stop words and punctuation
-##still working on removeing @
-for x in range(150):
-    text = json_load[x]["Source of Tweet"]
+def drawGraph(G:nx.Graph) ->None:
+    options = {
+        'node_color': 'grey',
+        'node_size': 10,
+        'linewidths': 0,
+        'width': 0.1,
+        'font_size':5,
+        #'with_labels':True
+    }
+    nx.draw(G, **options)
+    
+    degree_sequence = sorted((d for n, d in nx.degree(G)), reverse=True)
 
-    #removes links, twitter handles, and random words that start with numbers
-    result = " ".join(
-        filter(lambda word: not word.lower().startswith(rmv_list),
-               text.split()))
+    fig= plt.figure("ProVAX", figsize=(8,8))
+    axgrid = fig.add_gridspec(5,4)
 
-    #removed punctuation
-    result = re.sub(r'[^\w\s]', '', result)
+    ax2 = fig.add_subplot(axgrid[0:3, :])
+    ax2.hist(nx.closeness_centrality(G).values(),bins=160)
+    ax2.set_title("Closeness Centrality Histogram")
+    ax2.set_xlabel("Closeness")
+    ax2.set_ylabel("# of Nodes")
 
-    #separates every word and removes all stop words
-    text_tokens = word_tokenize(result.lower())
-    tokens_without_sw = [
-        word for word in text_tokens if not word in all_stopwords
-    ]
+    ax1 = fig.add_subplot(axgrid[3:, :2])
+    ax1.hist(nx.pagerank(G).values(), bins=70)
+    ax1.set_title("Page Ranking Histogram")
+    ax1.set_ylabel("# of Nodes")
+    ax1.set_xlabel("Rank")
 
-    #checks for english words // not sure to add because removes some real english words // about 20k words are removed
-    tokens_without_sw = [
-        w for w in tokens_without_sw if w.lower() in words or not w.isalpha()
-    ]
+    ax2 = fig.add_subplot(axgrid[3:, 2:])
+    ax2.bar(*np.unique(degree_sequence, return_counts=True))
+    ax2.set_title("Degree histogram")
+    ax2.set_xlabel("Degree")
+    ax2.set_ylabel("# of Nodes")
 
-    for i in range(len(tokens_without_sw)-1):
-        G.add_edge(tokens_without_sw[i], tokens_without_sw[i+1])
+    fig.tight_layout()
+    plt.show()
 
+def tweetPreproccessing(json_load:json) -> None:
+    nltk.download('stopwords')
+    nltk.download('punkt')
+    nltk.download('words')
 
-G.remove_edges_from(nx.selfloop_edges(G))
-print(G.number_of_nodes())
+    #list of words to remove if they start with these chars
+    sw_list = ['id','like','feel','im','likes','go','dont','us','theyre','got','going','#']
+    rmv_list = ('@', '/', 'http', '#','1','2', '3', '4', '5', '6', '7', '8', '9', '0')
 
+    all_stopwords = stopwords.words('english')
+    all_stopwords.extend(sw_list)
+    words = set(nltk.corpus.words.words())
 
-options = {
-    'node_color': 'grey',
-    'node_size': 10,
-    'linewidths': 0,
-    'width': 0.1,
-    'font_size':5,
-    #'with_labels':True
-}
-nx.draw(G, **options)
+    G = nx.Graph()
+    numOfNode = 0
+    x=0
+    while numOfNode < 250:
+        scentence = json_load[x]["Source of Tweet"]
 
+        #removes links, twitter handles, and random words that start with numbers
+        scentence = " ".join(
+            filter(lambda word: not word.lower().startswith(rmv_list),
+                   scentence.split()))
 
-degree_sequence = sorted((d for n, d in nx.degree(G)), reverse=True)
+        #removed punctuation
+        scentence = re.sub(r'[^\w\s]', '', scentence)
 
-fig= plt.figure("ProVAX", figsize=(8,8))
-axgrid = fig.add_gridspec(5,4)
+        #separates every word and removes all stop words
+        text_tokens = word_tokenize(scentence.lower())
+        tokens_without_sw = [
+            word for word in text_tokens if not word in all_stopwords
+        ]
 
-ax2 = fig.add_subplot(axgrid[0:3, :])
-ax2.hist(nx.closeness_centrality(G).values(),bins=160)
-ax2.set_title("Closeness Centrality Histogram")
-ax2.set_xlabel("Degree")
-ax2.set_ylabel("# of Nodes")
+        #checks for english words // not sure to add because removes some real english words // about 20k words are removed
+        tokens_without_sw = [
+            w for w in tokens_without_sw if w.lower() in words or not w.isalpha()
+        ]
 
-ax1 = fig.add_subplot(axgrid[3:, :2])
-ax1.hist(nx.pagerank(G).values(), bins=70)
-ax1.set_title("Pageranking Histogram")
-ax1.set_ylabel("Degree")
-ax1.set_xlabel("Rank")
+        for i in range(len(tokens_without_sw)-1):
+            G.add_edge(tokens_without_sw[i], tokens_without_sw[i+1])
+        numOfNode = G.number_of_nodes()
+        x = x + 1
+    
+    #print("number of tweets: " + str(x))
+    G.remove_edges_from(nx.selfloop_edges(G))
+    drawGraph(G)
 
-ax2 = fig.add_subplot(axgrid[3:, 2:])
-ax2.bar(*sp.unique(degree_sequence, return_counts=True))
-ax2.set_title("Degree histogram")
-ax2.set_xlabel("Degree")
-ax2.set_ylabel("# of Nodes")
+def tweetScraping(filter: str) -> None:
 
-fig.tight_layout()
-plt.show()
+    attributes_container = []
+    # Using TwitterSearchScraper to scrape data and append tweets to list
+    for i, tweet in enumerate(
+            sntwitter.TwitterSearchScraper(filter).get_items()):
+        if i > 300:
+            break
+        attributes_container.append([tweet.date, tweet.id, tweet.content, tweet.user.username])
+    # Creating a dataframe from the tweets list above
+    tweets_df = pd.DataFrame(attributes_container,
+                             columns=[
+                                 "Date Created", "Number of Likes",
+                                 "Source of Tweet", "Tweets"
+                             ])
+    tweets_df.to_json(filter + '.json', orient="records")
+
+    with open(filter + '.json', 'r') as json_file:
+        jsondata = json.load(json_file)
+
+    tweetPreproccessing(jsondata)
+
+tweetScraping('#VaccinesWork')
+tweetScraping('#NoVaxMandates')
